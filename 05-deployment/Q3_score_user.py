b2f02fe707e68285beb4d082afed561993144082
Nathalia Adriele <nat-adriele@hotmail.com>
2024-10-27 15:40:22 +0000

    Q3 score 1

---
+++ 05-deployment/Q3_score_user.py
@@ -0,0 +1,14 @@
+import pickle
+ 
+def load_model(name: str):
+    with open(name, "rb") as f_in:
+        return pickle.load(f_in)
+
+dv = load_dv(name="dv.bin")    
+model = load_model("model1.bin")
+
+user = {"job": "management", "duration": 400, "poutcome": "success"}
+
+X = dv.transform([user])
+y_pred = model.predict_proba(X)[0, 1]
+print(y_pred)
