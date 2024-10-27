fe24f74708a45bf53d399be7e5010c0667fbbf62
Nathalia Adriele <nat-adriele@hotmail.com>
2024-10-27 15:45:04 +0000

    def get_dv_model

--- 05-deployment/QA_score_user_web.py
+++ 05-deployment/QA_score_user_web.py
@@ -3,3 +3,10 @@ import os
 from flask import Flask, jsonify, request
 
 app = Flask("score_user")
+
+def get_dv_model():
+    dv = load_dv(name="dv.bin")    
+    model_name = os.getenv("MODEL_NAME", "model1.bin") 
+    print("model_name: ", model_name)
+    model = load_model(model_name)
+    return dv, modelin)
