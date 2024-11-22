eeb2a9356f4b79ae5365e4965d6a710a12a922b6
Nathalia Adriele <nat-adriele@hotmail.com>
2024-11-22 17:47:57 +0000

    update predict-test

--- midterm-project/predict-test.py
+++ midterm-project/predict-test.py
@@ -6,7 +6,7 @@ import requests
 url = 'http://localhost:9696/predict'
 
 patient_id = 'abc-123'
-patient = {
+patient1 = {
     "NumMedicalVisits": 5,
     "Cholesterol": 240,
     "BloodPressure": 130,
@@ -17,10 +17,29 @@ patient = {
     "Age": 45
 }
 
-response = requests.post(url, json=patient).json()
-print(response)
+patient2 = {
+    "NumMedicalVisits": 3,
+    "Cholesterol": 200,
+    "BloodPressure": 120,
+    "PhysicalActivity": 150,
+    "SodiumIntake": 2800,
+    "BMI": 25.1,
+    "HypertensionPedigreeFunction": 0.8,
+    "Age": 35
+}
 
-if response['hypertension']:
+response1 = requests.post(url, json=patient1).json()
+print(f"Response for patient {patient_id}: {response1}")
+
+if response1['hypertension']:
     print(f'Sending an appointment email to {patient_id}')
 else:
     print(f'NOT sending an appointment email to {patient_id}')
+
+response2 = requests.post(url, json=patient2).json()
+print(f"Response for patient 2: {response2}")
+
+if response2['hypertension']:
+    print('Sending an appointment email to patient 2')
+else:
+    print('NOT sending an appointment email to patient 2')
