b86902eab242e888e4cd76a967e3c29689ec607d
Nathalia Adriele <nat-adriele@hotmail.com>
2024-12-14 19:03:45 +0000

    test.py

---
+++ 01-capstone-project/test.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import joblib
+
+X1 = {
+  "model": "Fiesta",
+  "year": 2019,
+  "engineSize": 1.0,
+  "fuelType": "Petrol",
+  "transmission": "Manual",
+  "mileage": 10460,
+  "tax": 145,
+  "mpg": 40.3
+}
+
+X2 = {
+  "model": "Kuga",
+  "year": 2019,
+  "engineSize": 2.0,
+  "fuelType": "Diesel",
+  "transmission": "Automatic",
+  "mileage": 6894,
+  "tax": 145,
+  "mpg": 42.2
+}
