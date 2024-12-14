f493efb66c57f0eb1ae7d73d8a24fcc587b57c8b
Nathalia Adriele <nat-adriele@hotmail.com>
2024-12-14 19:10:49 +0000

    test

--- 01-capstone-project/test.py
+++ 01-capstone-project/test.py
@@ -2,24 +2,31 @@ import pandas as pd
 import numpy as np
 import joblib
 
+pipeline = joblib.load("pipeline.joblib")
+
 X1 = {
-  "model": "Fiesta",
-  "year": 2019,
-  "engineSize": 1.0,
-  "fuelType": "Petrol",
-  "transmission": "Manual",
-  "mileage": 10460,
-  "tax": 145,
-  "mpg": 40.3
+    "model": "Fiesta",
+    "year": 2019,
+    "transmission": "Manual",
+    "mileage": 3265,
+    "fueltype": "Petrol",
+    "tax": 145,
+    "mpg": 58.9,
+    "enginesize": 1
 }
 
 X2 = {
-  "model": "Kuga",
-  "year": 2019,
-  "engineSize": 2.0,
-  "fuelType": "Diesel",
-  "transmission": "Automatic",
-  "mileage": 6894,
-  "tax": 145,
-  "mpg": 42.2
+    "model": "Focus",
+    "year": 2018,
+    "transmission": "Manual",
+    "mileage": 9083,
+    "fueltype": "Petrol",
+    "tax": 150,
+    "mpg": 57.7,
+    "enginesize": 1
 }
+
+df_test = pd.DataFrame([X1, X2])
+predictions = pipeline.predict(df_test)
+print("X1 Predicted Price:", predictions[0])
+print("X2 Predicted Price:", predictions[1])
